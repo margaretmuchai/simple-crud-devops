aeb509a1241d5e4d2b3afc3d7c32eeae25f81856
margaretmuchai <margaretwanjiru@adanian.io>
2026-04-10 12:57:48 +0300

    Added docker files

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://frontend:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
